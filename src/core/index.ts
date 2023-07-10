315a88b16aa983ccb819293b0594dfc620967d49
Cauchy03 <1481856337@qq.com>
2023-07-10 15:42:38 +0800

    :zkles: feat: add UV and report

--- src/core/index.ts
+++ src/core/index.ts
@@ -23,11 +23,30 @@ export default class Tracker {
     }
   }
 
+  public setUerId<T extends DefaultOptions['uuid']>(uuid: T) {
+    this.data.uuid = uuid
+  }
+
+  public setExtra<T extends DefaultOptions['extra']>(extra: T) {
+    this.data.extra = extra
+  }
+
+  // 用户手动上报
+  public sendTracker<T>(data: T) {
+    this.reportTracker({data})
+  }
+
   // 捕获器 监听事件函数
   private captureEvent<T>(mouseEventList: string[], targetKey: string, data?: T) {
     mouseEventList.forEach(event => {
       window.addEventListener(event, () => {
         console.log('监听到了', event)
+        // 自动上报
+        this.reportTracker({
+          event,
+          targetKey,
+          data
+        })
       })
     })
   }
@@ -35,10 +54,21 @@ export default class Tracker {
   // 初始化函数
   private installTracker() {
     if (this.data.historyTracker) {
-      this.captureEvent(['pushState', 'replaceState', 'popstate'],'history-pv')
+      this.captureEvent(['pushState', 'replaceState', 'popstate'], 'history-pv')
     }
     if (this.data.hashTracker) {
-      this.captureEvent(['hashchange'],'hash-pv')
+      this.captureEvent(['hashchange'], 'hash-pv')
     }
   }
+
+  // 上报后台
+  private reportTracker<T>(data: T) {
+    const params = Object.assign(this.data, data, { time: new Date().getTime() })
+    let headers = {
+      type: 'application/x-www-form-urlencoded'
+    }
+    let blob = new Blob([JSON.stringify(params)], headers)
+    navigator.sendBeacon(this.data.requestUrl, blob) // navigator.sendBeacon() 只能传post
+  }
+
 }
